ba6b0f11b24ce132ac5638de64f064fb328e2cca
ps1978 <akapaulo@hotmail.com>
2016-09-30 15:02:09 +0100

    final

--- Menus/www/js/mapas.js
+++ Menus/www/js/mapas.js
@@ -16,59 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
- function getPosition() {
-
-   var options = {
-      enableHighAccuracy: true,
-      maximumAge: 3600000
-   }
-    
-   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
-
-   function onSuccess(position) {
-
-      alert('Latitude: '         + position.coords.latitude          + '\n' +
-         'Longitude: '         + position.coords.longitude         + '\n' +
-         'Altitude: '          + position.coords.altitude          + '\n' +
-         'Accuracy: '          + position.coords.accuracy          + '\n' +
-         'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-         'Heading: '           + position.coords.heading           + '\n' +
-         'Speed: '             + position.coords.speed             + '\n' +
-         'Timestamp: '         + position.timestamp                + '\n');
-   };
-
-   function onError(error) {
-      alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-   }
-}
-
-function watchPosition() {
-
-   var options = {
-      maximumAge: 3600000,
-      timeout: 3000,
-      enableHighAccuracy: true,
-   }
-
-   var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
-
-   function onSuccess(position) {
-
-      alert('Latitude: '          + position.coords.latitude          + '\n' +
-         'Longitude: '         + position.coords.longitude         + '\n' +
-         'Altitude: '          + position.coords.altitude          + '\n' +
-         'Accuracy: '          + position.coords.accuracy          + '\n' +
-         'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-         'Heading: '           + position.coords.heading           + '\n' +
-         'Speed: '             + position.coords.speed             + '\n' +
-         'Timestamp: '         + position.timestamp                + '\n');
-   };
-
-   function onError(error) {
-      alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
-   }
-
-}
 var app = {
     // Application Constructor
     initialize:function(){
@@ -87,9 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
   //chamamos o método getCurrentPosition, que se encaixe no nosso GPS dispositivo e descobre onde estamos. 
-        //navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
-    document.getElementById("getPosition").addEventListener("click", getPosition);
-    document.getElementById("watchPosition").addEventListener("click", watchPosition);
+        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
     },
   //O método onSuccess, e o objecto é passado para a posição-o.
    onSuccess: function(position){ 
@@ -112,4 +57,15 @@ var app = {
    onError:function(error){
     alert('code:' +error.code +'\n' + 'message:' + error.message + '\n');
    },
+    // Update DOM on a Received Event
+    /*receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }*/
 };
